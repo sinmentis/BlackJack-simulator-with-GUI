9d17cd5dede421804a35f68b2eff2b68d5019db8
Sinmentis <Sinmentis@foxmail.com>
2020-02-10 19:49:26 +1300

    Update Class Card Deck Player

--- blackjack_sim.py
+++ blackjack_sim.py
@@ -1,15 +1,30 @@
 # -*- coding:utf-8 -*-
 import sys
+from Deck_Card import Card, Deck, Player
 from PySide2 import QtCore, QtGui, QtWidgets
 
-app = QtWidgets.QApplication(sys.argv)
+def main():
+    app = QtWidgets.QApplication(sys.argv)
 
-quit = QtWidgets.QPushButton("Quit")
-quit.resize(75, 30)
-quit.setFont(QtGui.QFont("Times", 18, QtGui.QFont.Bold))
+    quit = QtWidgets.QPushButton("Quit")
+    quit.resize(75, 30)
+    quit.setFont(QtGui.QFont("Times", 18, QtGui.QFont.Bold))
 
-QtCore.QObject.connect(quit, QtCore.SIGNAL("clicked()"),
-                       app, QtCore.SLOT("quit()"))
+    QtCore.QObject.connect(quit, QtCore.SIGNAL("clicked()"),
+                           app, QtCore.SLOT("quit()"))
 
-quit.show()
-sys.exit(app.exec_())
+    quit.show()
+    sys.exit(app.exec_())
+
+
+def test():
+    player = Player("Shun")
+    deck = Deck()
+    deck.shuffle()
+    player.get_card(deck.draw())
+    player.show_hand()
+
+
+if __name__ == "__main__":
+    # main()
+    test()
