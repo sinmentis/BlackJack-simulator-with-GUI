baab206f124599844adc2a40f32ee04cc961657f
Sinmentis <Sinmentis@foxmail.com>
2020-02-10 19:55:38 +1300

    Bring up Windows Frame

--- Deck_Card_Player.py
+++ Deck_Card_Player.py
@@ -36,6 +36,7 @@ class Card:
 class Player:
     def __init__(self, name):
         self.name = name
+        self.score = 0
         self.hand = []
 
     def get_card(self, card):
---
+++ game.py
@@ -0,0 +1,29 @@
+from Deck_Card_Player import Card, Deck, Player
+from tkinter import *
+
+RESOLUTION = "500x600"
+
+
+def GUI_init(window):
+    window.title("BlackJack Simulator")
+    width_limit = (window.winfo_screenwidth() - int(RESOLUTION[:RESOLUTION.find("x")])) / 2
+    height_limit = (window.winfo_screenheight() - int(RESOLUTION[RESOLUTION.find("x") + 1:])) / 2
+    window.geometry("{0}+{1:.0f}+{2:.0f}".format(RESOLUTION, width_limit, height_limit))
+
+
+def init_game():
+    player = Player("Shun")
+    deck = Deck()
+    deck.shuffle()
+    return player, deck
+
+
+def main():
+    window = Tk()
+    player, deck = init_game()
+    GUI_init(window)
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
